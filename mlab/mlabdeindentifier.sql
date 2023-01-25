83ae1eba113bd8e42b1a052d1ff735beb508798f
Joshua Murithi <37470288+Murithijoshua@users.noreply.github.com>
2023-01-25 15:01:22 +0300

    Update mlabdeindentifier.sql

--- mlab/mlabdeindentifier.sql
+++ mlab/mlabdeindentifier.sql
@@ -3,7 +3,12 @@ Author: Joshua Murithi
 Github: https://github.com/murithijoshua
 Function: Mlab de-identifier script
 **/
---  random client ID, and random created_at and Updated_at
+/** 
+randomizing client ID,
+reducing the  created_at and Updated_at dates by 5 years
+
+
+**/
 update results
 set created_at=created_at - interval '5 years',
 updated_at=updated_at-interval '5 years',
